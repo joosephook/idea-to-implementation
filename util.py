dd49b8d3e6c9beef1cffea766c9c58ca80f4dadc
joosep <joosephook@gmail.com>
2021-10-04 20:25:37 +0300

    remove dead code

--- util.py
+++ util.py
@@ -1,13 +1,11 @@
-import numpy as np
 import torch
-import torchvision
-from matplotlib import pyplot as plt
 from sklearn.metrics import confusion_matrix
 from torch.nn import functional as F
 
 
 def train(model, optimizer, train_loader, device):
-    # modified from:
+    # Author: Ghassen Hamrouni
+    # Modified for the Veriff Task by: Joosep Hook
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
@@ -32,8 +30,8 @@ def train(model, optimizer, train_loader, device):
 
 
 def test(model, test_loader, device):
-    # modified from:
-
+    # Author: Ghassen Hamrouni
+    # Modified for the Veriff Task by: Joosep Hook
     with torch.no_grad():
         model.eval()
         test_loss = 0
@@ -55,51 +53,6 @@ def test(model, test_loader, device):
         cm = confusion_matrix(torch.cat(targets).cpu(), torch.cat(preds).cpu())
         test_loss /= len(test_loader.dataset)
         return test_loss, correct / len(test_loader.dataset), cm
-
-
-def convert_image_np(inp):
-    ######################################################################
-    # Visualizing the STN results
-    # ---------------------------
-    #
-    # Now, we will inspect the results of our learned visual attention
-    # mechanism.
-    #
-    # We define a small helper function in order to visualize the
-    # transformations while training.
-    """Convert a Tensor to numpy image."""
-    inp = inp.numpy().transpose((1, 2, 0))
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    inp = std * inp + mean
-    inp = np.clip(inp, 0, 1)
-    return inp
-
-
-def visualize_stn(model, test_loader, device):
-    # We want to visualize the output of the spatial transformers layer
-    # after the training, we visualize a batch of input images and
-    # the corresponding transformed batch using STN.
-    with torch.no_grad():
-        # Get a batch of training data
-        data = next(iter(test_loader))[0].to(device)
-
-        input_tensor = data.cpu()
-        transformed_input_tensor = model.stn(data).cpu()
-
-        in_grid = convert_image_np(
-            torchvision.utils.make_grid(input_tensor))
-
-        out_grid = convert_image_np(
-            torchvision.utils.make_grid(transformed_input_tensor))
-
-        # Plot the results side-by-side
-        f, axarr = plt.subplots(1, 2)
-        axarr[0].imshow(in_grid)
-        axarr[0].set_title('Dataset Images')
-
-        axarr[1].imshow(out_grid)
-        axarr[1].set_title('Transformed Images')
 
 
 def reproducible_ops() -> torch.device:
